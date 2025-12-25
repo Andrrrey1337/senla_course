e2d3a5538d561775893ec984eb428f37cde6ba61
Andrey Ryabikov <anryabickov@yandex.ru>
2025-12-25 23:48:50 +0300

    task-10

--- task-9/task_4/StartTime.java
+++
@@ -1,9 +0,0 @@
-package task_4;
-
-public class StartTime {
-    public static void main(String[] args) throws InterruptedException{
-        TimeDaemonThread timeThread = new TimeDaemonThread(2);
-        timeThread.start();
-        Thread.sleep(10000);
-    }
-}
