362508a3ba3d049fc8cb1797aa8071cf98961457
Andrey Ryabikov <anryabickov@yandex.ru>
2026-02-02 22:09:23 +0300

    task-14 correct

--- src/main/java/task_1/annotations/Component.java
+++
@@ -1,7 +0,0 @@
-package task_1.annotations;
-
-import java.lang.annotation.*;
-
-@Target(value = ElementType.TYPE)
-@Retention(value = RetentionPolicy.RUNTIME)
-public @interface Component { }
--- src/main/java/task_1/annotations/Inject.java
+++
@@ -1,7 +0,0 @@
-package task_1.annotations;
-
-import java.lang.annotation.*;
-
-@Target(value = ElementType.FIELD)
-@Retention(value = RetentionPolicy.RUNTIME)
-public @interface Inject { }
